46aeb8d853d30866fbe8e18c752882b855904526
Beltrame03 <48033189+Beltrame03@users.noreply.github.com>
2020-02-15 09:04:17 -0500

    Add files via upload

---
+++ main/app.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+#from refresh import *
+from userInfo import *
+from id import *
+
+user = NewUser("33aas")
+'''
+print(user)'''
+
+
+'''
+root = tk.Tk()
+root.geometry("500x500")
+root.configure(bg = "black")
+
+root.title("Dashboard")
+
+text = ["hiu", "bye"]
+for i in range(len(text)):
+    w = tk.Label(root, text=text[i], bg = "black", fg = "white")
+    w.pack()
+
+root.mainloop()
+'''
